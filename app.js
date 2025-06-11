6f3da50d56b4c213d2a51d76352b03dff44a25b3
aamirjawed <aamirjawed682@gmail.com>
2025-06-11 15:09:29 +0530

    made mulitple routes

--- app.js
+++ app.js
@@ -4,13 +4,36 @@ const app  = express()
 
 const router = express.Router();
 
-function addUserMiddleware(req, res, next){
-    req.user ="Guest"
-    next()
-}
+app.use(router)
 
-app.get("/welcome", addUserMiddleware, (req,res) => {
-    res.send(`<h1>Welcome ${req.user}!</h1>`)
+// function addUserMiddleware(req, res, next){
+//     req.user ="Guest"
+//     next()
+// }
+
+// app.get("/welcome", addUserMiddleware, (req,res) => {
+//     res.send(`<h1>Welcome ${req.user}!</h1>`)
+// })
+
+// making routes 
+
+router.get("/orders", (req,res) => {
+    res.send("Here is the list of all orders")
+    console.log("All orders list")
+})
+
+router.post("/order", (req, res) => {
+    res.send("A new order has been created.")
+    console.log("A new order has been created")
+})
+
+router.get("/users", (req, res) =>{
+    res.send("Here is the list of all users")
+    console.log("All users list")
+})
+
+router.post("/user", (req,res) => {
+    res.send("A new user has been added.")
 })
 
 app.listen(3000, () => {
