1a30146fc79e993ba0a8205effcecbb22a877556
aamirjawed <aamirjawed682@gmail.com>
2025-06-11 14:57:45 +0530

    made middleware and routes

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require("express")
 
 const app  = express()
 
+const router = express.Router();
+
+function addUserMiddleware(req, res, next){
+    req.user ="Guest"
+    next()
+}
+
+app.get("/welcome", addUserMiddleware, (req,res) => {
+    res.send(`<h1>Welcome ${req.user}!</h1>`)
+})
+
 app.listen(3000, () => {
     console.log("Server is up and running on port 3000! Ready to handle requests.")
 })
