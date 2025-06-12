5bc516e6f7e753bec07dc466717c2915676873b4
aamirjawed <aamirjawed682@gmail.com>
2025-06-12 11:01:28 +0530

    build dynamic routing and used query params

--- app.js
+++ app.js
@@ -37,24 +37,41 @@ app.use(router)
 // })
 
 
-router.get("/products", (req, res) => {
-    res.send("Here is the list of all products.")
+// router.get("/products", (req, res) => {
+//     res.send("Here is the list of all products.")
+// })
+
+// router.post("/products", (req,res) => {
+//     console.log("A new product has been added.")
+// })
+
+// router.get("/categories", (req,res) => {
+//     res.send("Here is the list of all categories")
+// })
+
+// router.get("/categories", (req, res) => {
+//     console.log("A new category has been created.")
+// })
+
+// app.use((req,res) => {
+//     res.status(404).send("<h1>404- Page not found</h1>")
+// })
+
+// dynamic routing
+
+router.get("/", (req, res) => {
+    res.send("Hello from Home page!")
 })
 
-router.post("/products", (req,res) => {
-    console.log("A new product has been added.")
+router.get("/about", (req, res) => {
+    res.send("Hello from About page!")
 })
 
-router.get("/categories", (req,res) => {
-    res.send("Here is the list of all categories")
-})
-
-router.get("/categories", (req, res) => {
-    console.log("A new category has been created.")
-})
-
-app.use((req,res) => {
-    res.status(404).send("<h1>404- Page not found</h1>")
+router.get("/contact/:username", (req, res)=>{
+    
+    const name = req.params.username
+    const role = req.query.role
+    res.send(`Welcome ${name} your role is ${role}`)
 })
 
 app.listen(3000, () => {
